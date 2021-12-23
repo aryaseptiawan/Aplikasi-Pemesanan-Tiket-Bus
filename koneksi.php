0c5b8c4b14a44956e7e2a34308316f13dd322031
aryaseptiawan <aryaseptiawan18@gmail.com>
2021-12-23 19:29:12 +0700

    fix database

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,7 @@ session_start();
 $host       =   "localhost";
 $user       =   "root";
 $password   =   "";
-$database   =   "umn-bus";
+$database   =   "umnbus";
 // perintah php untuk akses ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
 ?>
